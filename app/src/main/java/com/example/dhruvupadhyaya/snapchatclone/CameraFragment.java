157e932b79fac2b736ccee5a3ff2c338b73b80ed
DhruvUpadhyaya <40164639+DhruvUpadhyaya@users.noreply.github.com>
2018-08-04 15:31:54 +0530

    Fragments Done !!

--- app/src/main/java/com/example/dhruvupadhyaya/snapchatclone/CameraFragment.java
+++ app/src/main/java/com/example/dhruvupadhyaya/snapchatclone/CameraFragment.java
@@ -1,12 +1,30 @@
 package com.example.dhruvupadhyaya.snapchatclone;
 
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class CameraFragment extends Fragment {
+import java.io.IOException;
+
+public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
+
+    Camera camera ;
+
+    final int CAMERA_REQUEST_CODE = 1;
+
+    SurfaceView mSurfaceView;
+    SurfaceHolder mSurfaceHolder;
+
     public static CameraFragment newInstance(){
         CameraFragment fragment = new CameraFragment();
         return fragment;
@@ -16,9 +34,70 @@ public class CameraFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-
         View view = inflater.inflate(R.layout.fragment_camera,container,false);
+
+        mSurfaceHolder = mSurfaceView.getHolder();
+
+        if (ActivityCompat.checkSelfPermission(getContext(),android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(),new String[] {android.Manifest.permission.CAMERA},CAMERA_REQUEST_CODE);
+        }else{
+            mSurfaceHolder.addCallback(this);
+            mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        }
+
+
+
+
+
+
         return view;
 
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        camera = Camera.open();
+
+        Camera.Parameters parameters;
+        parameters = camera.getParameters();
+
+        camera.setDisplayOrientation(90);
+        parameters.setPreviewFrameRate(30);
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        camera.startPreview();
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case  CAMERA_REQUEST_CODE:{
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    mSurfaceHolder.addCallback(this);
+                    mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+                }else {
+                    Toast.makeText(getContext(), "Please provide the permission", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
     }
 }
